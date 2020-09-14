7981e81fd292c9813999dd9078306b2d413dcdbc
Kartikeya Shukla <31617919+KartikeyaShukla911@users.noreply.github.com>
2020-09-14 23:19:46 +0530

    Add files via upload

---
+++ Kartikeya/C++_by_Kartikeya.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello Word! My name is Kartikeya Shukla"<<endl;
+	return 0;
+}
---
+++ Kartikeya/C_by_Kartikeya.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World! My name is Kartikeya Shukla\n");
+	return 0;
+}
